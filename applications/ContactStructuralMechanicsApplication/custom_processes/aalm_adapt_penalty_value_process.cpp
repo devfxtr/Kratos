3732ed57e1ef526496f942d857859a0d1c5276c7
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-02-24 11:13:02 +0100

    Adding check to aalm process

--- applications/ContactStructuralMechanicsApplication/custom_processes/aalm_adapt_penalty_value_process.cpp
+++ applications/ContactStructuralMechanicsApplication/custom_processes/aalm_adapt_penalty_value_process.cpp
@@ -42,34 +42,38 @@ void AALMAdaptPenaltyValueProcess::Execute()
         
         // Weighted values
         const double nodal_area = it_node->GetValue(NODAL_AREA);
-        const double current_gap = it_node->FastGetSolutionStepValue(WEIGHTED_GAP)/nodal_area;
-        const double previous_gap = it_node->FastGetSolutionStepValue(WEIGHTED_GAP, 1)/nodal_area;
-        
-        // Nodal H x gap factor
-        const double max_gap = max_gap_factor * it_node->FastGetSolutionStepValue(NODAL_H); // NOTE: This value must be studied
-        
-//         // DEBUG
-//         KRATOS_INFO("Current gap") << "current_gap: " << current_gap << "\tprevious_gap: " << previous_gap << "\tmax_gap: " << max_gap << std::endl;
-//         KRATOS_INFO("Sign change gaps") << "(current_gap * previous_gap): " << (current_gap * previous_gap) << std::endl;
-//         KRATOS_INFO("Compare previous gap") << "std::abs(previous_gap) > max_gap: " << std::abs(previous_gap) > max_gap << std::endl;
-//         KRATOS_INFO("Compare current gap") << "std::abs(current_gap) > max_gap: " << std::abs(current_gap) > max_gap << std::endl;
-        
-        if ((current_gap * previous_gap) < 0.0) {
-            if (std::abs(previous_gap) > max_gap) // NOTE: The abs is deduced from the paper (the algorithm is without abs)
-                it_node->SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (current_gap) * (std::abs(current_gap) + max_gap)/(current_gap - previous_gap)));
-            else
-                it_node->SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (10.0 * current_gap)));
-        } else if (std::abs(current_gap) > max_gap) { // NOTE: The abs is deduced from the paper (the algorithm is without abs)
-            if (std::abs(current_gap - previous_gap) > std::max(current_gap/10.0, std::max(previous_gap/1.0, 5 * max_gap)))
-                it_node->SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter);
-            else if ((std::abs(current_gap) <= std::abs(previous_gap) * 1.01 || std::abs(current_gap) >= std::abs(previous_gap) * 0.99) && (std::abs(current_gap) < 10.0 *  max_gap))
-                it_node->SetValue(INITIAL_PENALTY, penalty_parameter * std::pow((std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0), 2.0));
-            else if (std::abs(current_gap) > std::abs(previous_gap) * 1.01)
-                it_node->SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter * (previous_gap/current_gap));
-            else
-                it_node->SetValue(INITIAL_PENALTY, penalty_parameter * (std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0));
-        } else
-            it_node->SetValue(INITIAL_PENALTY, penalty_parameter);
+        if (nodal_area > std::numeric_limits<double>::epsilon()) {
+            const double current_gap = it_node->FastGetSolutionStepValue(WEIGHTED_GAP)/nodal_area;
+            const double previous_gap = it_node->FastGetSolutionStepValue(WEIGHTED_GAP, 1)/nodal_area;
+            
+            // Nodal H x gap factor
+            const double max_gap = max_gap_factor * it_node->FastGetSolutionStepValue(NODAL_H); // NOTE: This value must be studied
+            
+            // DEBUG
+            KRATOS_TRACE("Current gap") << "current_gap: " << current_gap << "\tprevious_gap: " << previous_gap << "\tmax_gap: " << max_gap << std::endl;
+            KRATOS_TRACE("Sign change gaps") << "(current_gap * previous_gap): " << (current_gap * previous_gap) << std::endl;
+            KRATOS_TRACE("Compare previous gap") << "std::abs(previous_gap) > max_gap: " << (std::abs(previous_gap) > max_gap) << std::endl;
+            KRATOS_TRACE("Compare current gap") << "std::abs(current_gap) > max_gap: " << (std::abs(current_gap) > max_gap) << std::endl;
+            
+            if ((current_gap * previous_gap) < 0.0) {
+                if (std::abs(previous_gap) > max_gap) // NOTE: The abs is deduced from the paper (the algorithm is without abs)
+                    it_node->SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (current_gap) * (std::abs(current_gap) + max_gap)/(current_gap - previous_gap)));
+                else
+                    it_node->SetValue(INITIAL_PENALTY, std::abs(penalty_parameter * previous_gap / (10.0 * current_gap)));
+            } else if (std::abs(current_gap) > max_gap) { // NOTE: The abs is deduced from the paper (the algorithm is without abs)
+                if (std::abs(current_gap - previous_gap) > std::max(current_gap/10.0, std::max(previous_gap/1.0, 5 * max_gap)))
+                    it_node->SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter);
+                else if ((std::abs(current_gap) <= std::abs(previous_gap) * 1.01 || std::abs(current_gap) >= std::abs(previous_gap) * 0.99) && (std::abs(current_gap) < 10.0 *  max_gap))
+                    it_node->SetValue(INITIAL_PENALTY, penalty_parameter * std::pow((std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0), 2.0));
+                else if (std::abs(current_gap) > std::abs(previous_gap) * 1.01)
+                    it_node->SetValue(INITIAL_PENALTY, 2.0 * penalty_parameter * (previous_gap/current_gap));
+                else
+                    it_node->SetValue(INITIAL_PENALTY, penalty_parameter * (std::sqrt(std::abs(current_gap)/max_gap - 1.0) + 1.0));
+            } else
+                it_node->SetValue(INITIAL_PENALTY, penalty_parameter);
+            } else {
+                it_node->SetValue(INITIAL_PENALTY, penalty_parameter);
+        }
     }
 
     KRATOS_CATCH("");
